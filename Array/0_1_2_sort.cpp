c54c94ed0aa969a9c93610cecfe354f578b6d2a8
BISHWEASHWAR SUKLA <44729114+bishweashwarsukla@users.noreply.github.com>
2021-07-22 22:28:04 +0530

    Add files via upload

---
+++ Array/0_1_2_sort.cpp
@@ -0,0 +1,28 @@
+//Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector <int > v={0,1,0,1,2,1};
+
+    int temp=0;
+    for(int i=0;i<v.size()-1;i++)
+    {
+        for(int j=i+1;j<v.size();j++)
+        {
+            if(v[i]>v[j])
+            {
+                temp=v[j];
+                v[j]=v[i];
+                v[i]=temp;
+            }
+        }
+    }
+
+    for(auto x:v)
+    {
+        cout<<x<<" ";
+    }
+
+    return 0;
+}
---
+++ Array/duplicate_in_arr.cpp
@@ -0,0 +1,43 @@
+/*
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+Example 3:
+
+Input: nums = [1,1]
+Output: 1
+Example 4:
+
+Input: nums = [1,1,2]
+Output: 1
+*/
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) 
+    {
+        
+        int k=0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            { k=nums[i]; }
+        }
+        
+    }
+    return k;
+};
